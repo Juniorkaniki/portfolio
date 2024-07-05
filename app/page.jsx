39ed77d00b8bbd1867687548681e3e110a0b52bd
Juniorkaniki <juniorkaniki00@gmail.com>
2024-07-05 14:08:07 +0200

    begin hom page

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,26 @@
+import {Button} from "@/components/ui/button";
 
-export default function Home() {
-  return <main>homepage</main>
-}
+import {FilDownloard} from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justeify-between xl:pt-8 xl:pd-24">
+          {/* text*/}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software  Developper</span>
+            <h1  className="h1">
+              hello I'm <br /> Kaniki Junior
+            </h1>
+            <p className="max-w-[500px] ">I excel at crafting elegant digital experiences and I am proficient in various programming languages amd technogies</p>
+          </div>
+        </div>
+
+      </div>
+ 
+    </section>
+  );
+};
+
+export default Home;
