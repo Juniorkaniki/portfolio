eb1bfee302e5d27aea8cb9f23b391b24ca09b4b7
Juniorkaniki <juniorkaniki00@gmail.com>
2024-07-05 13:20:45 +0200

    fin mobile nav

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,13 +1,12 @@
-
 "use client";
 
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Import } from "lucide-react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import {CiMenuFries} from "react-icons/ci";
+import { CiMenuFries } from "react-icons/ci";
 
-const links =[
+const links = [
     { 
         name: 'home',
         path: '/'
@@ -30,24 +29,35 @@ const links =[
     },   
 ];
 
- 
 const MobileNav = () => {
     const pathname = usePathname();
     return (
         <Sheet>
             <SheetTrigger className="flex justify-center items-center">
                 <CiMenuFries className="text-[32px] text-accent"/>
-
             </SheetTrigger>
             <SheetContent className="flex flex-col">
-                {/*logo */}
-                <div>logo</div>
-
+                {/* Logo */}
+                <div className="mt-20 mb-20 text-center text-2xl">
+                    <Link href="/">
+                        <h1 className="text-3xl font-semibold">Kanik J<span className="text-accent">.</span></h1>
+                    </Link>
+                </div>
+                {/* Nav */}
+                <nav className="flex flex-col justify-center items-center gap-8">
+                    {links.map((link, index) => (
+                        <Link 
+                            href={link.path} 
+                            key={index} 
+                            className={`text-xl capitalize hover:text-accent transition-all ${link.path === pathname ? 'text-accent border-b-2 border-accent' : ''}`}
+                        >
+                            {link.name}
+                        </Link>
+                    ))}
+                </nav>
             </SheetContent>
         </Sheet>
     );
-    
-    <nav>MobileNav</nav>;
 };
 
-export default  MobileNav;
+export default MobileNav;
