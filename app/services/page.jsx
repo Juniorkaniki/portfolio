ba5083b326bbb31e6d78ed5197e371135c782b12
Juniorkaniki <juniorkaniki00@gmail.com>
2024-07-06 22:10:41 +0200

    services

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,8 +1,47 @@
  "use client ";
 
- import {} from 'react-icons';
+ import { BsArrowDownRight } from 'react-icons';
+ import Link from 'next/link';
+
+ const service =[
+    {
+        num: '01',
+        title:"Web Development",
+        description:" Lorem ipsum dolor sit, amet consectetur adipisicing elit. ",
+        href: ""
+        
+    },
+    {
+        num: '02',
+        title:"UI/UX Design ",
+        description:" Lorem ipsum dolor sit, amet consectetur adipisicing elit. ",
+         href: ""
+        
+    },
+    {
+        num: '03',
+        title:"Logo Design",
+        description:" Lorem ipsum dolor sit, amet consectetur adipisicing elit. ",
+         href: ""
+        
+    },
+    {
+        num: '04',
+        title:"SEO",
+        description:" Lorem ipsum dolor sit, amet consectetur adipisicing elit. ",
+         href: ""
+        
+    },
+ ]
+ import { motion } from 'framer-motion';
+
 const services = () => {
-    return <div>Services page</div>;
+    return (<section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+        <div className="container mx-auto">
+            <motion.div initial={{opacity:0}} animate={{opacity: 1, transition: {delay: 2.4, duration: 0.4, ease: "easeIn" },}}></motion.div>
+            </div>
+        </section>
+        );
 };
 
 export default services;
