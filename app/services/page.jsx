261aaf32729289b7973b47b525239561ab76d4eb
Juniorkaniki <juniorkaniki00@gmail.com>
2024-07-08 11:28:21 +0200

    Debut services

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,47 +1,70 @@
- "use client ";
+"use client";
 
- import { BsArrowDownRight } from 'react-icons';
- import Link from 'next/link';
+import { BsArrowDownRight } from 'react-icons/bs'; // Corrected import statement
+import Link from 'next/link';
+import { motion } from 'framer-motion';
 
- const service =[
-    {
-        num: '01',
-        title:"Web Development",
-        description:" Lorem ipsum dolor sit, amet consectetur adipisicing elit. ",
-        href: ""
-        
-    },
-    {
-        num: '02',
-        title:"UI/UX Design ",
-        description:" Lorem ipsum dolor sit, amet consectetur adipisicing elit. ",
-         href: ""
-        
-    },
-    {
-        num: '03',
-        title:"Logo Design",
-        description:" Lorem ipsum dolor sit, amet consectetur adipisicing elit. ",
-         href: ""
-        
-    },
-    {
-        num: '04',
-        title:"SEO",
-        description:" Lorem ipsum dolor sit, amet consectetur adipisicing elit. ",
-         href: ""
-        
-    },
- ]
- import { motion } from 'framer-motion';
+const servicesData = [
+  {
+    num: '01',
+    title: "Web Development",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+    href: ""
+  },
+  {
+    num: '02',
+    title: "UI/UX Design",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+    href: ""
+  },
+  {
+    num: '03',
+    title: "Logo Design",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+    href: ""
+  },
+  {
+    num: '04',
+    title: "SEO",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+    href: ""
+  },
+];
 
-const services = () => {
-    return (<section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
-        <div className="container mx-auto">
-            <motion.div initial={{opacity:0}} animate={{opacity: 1, transition: {delay: 2.4, duration: 0.4, ease: "easeIn" },}}></motion.div>
-            </div>
-        </section>
-        );
+const Services = () => {
+  return (
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+      <div className="container mx-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+          }}
+          className="grid gird-cols-1 md:grid-cols-2 gap-[60px]"
+        >
+          {servicesData.map((service, index) => {
+            return (
+              <div key={index}>
+                <div>
+                  <div>{service.num}</div>
+                  <Link href={service.href}>
+                    <BsArrowDownRight />
+                  </Link>
+                </div>
+                {/*title*/}
+                <h2>{service.title}</h2>
+                {/*description*/}
+                <p>{service.description}</p>
+                {/*border*/}
+                <div className="border-b border-white/20 w-full"></div>
+              </div>
+            );
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
 };
 
-export default services;
+export default Services;
