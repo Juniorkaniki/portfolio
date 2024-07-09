b62ee5870df13bb7ffd2a139bf9212a7b9a08a03
Juniorkaniki <juniorkaniki00@gmail.com>
2024-07-09 11:41:04 +0200

    resume

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,8 +1,10 @@
-"use client ";
+"use client";
 
-import { Info } from "lucide-react";
-import { FaHtml5, FaJs, FaCss3, FaFigma, FaNodeJs, FaFlu, FaDart,FaFlutter,FaCsharp, FaReact } from "react-icons/fa";
+import React, { createContext } from 'react';
+import { Component, Info } from "lucide-react";
+import { FaHtml5, FaJs, FaCss3, FaFigma, FaNodeJs, FaFlutter, FaDart, FaCsharp, FaReact } from "react-icons/fa";
 import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+
 const about = {
   title: "About me ",
   description:
@@ -21,7 +23,7 @@ const about = {
       fieldValue: "3+ Ans",
     },
     {
-      fieldName: "Skpe",
+      fieldName: "Skype",
       fieldValue: "Junior.243",
     },
     {
@@ -37,95 +39,98 @@ const about = {
       fieldValue: "Disponibles",
     },
     {
-      fieldName: "Lanngues",
-      fieldValue: "Français, Anglais,Swahilli",
+      fieldName: "Langues",
+      fieldValue: "Français, Anglais, Swahili",
     },
   ],
 };
-//experineces data
+
+// experiences data
 const experience = {
   icon: "/assets/resume/badge.svg",
-  title: "My Experiences ",
+  title: "My Experiences",
   description:
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil mollitia excepturi, corporis amet fugit voluptate, in quaerat dolores, nulla magni hic aspernatur velit aliquam atque repellat provident autem deserunt accusantium?",
   items: [
     {
       company: "RME",
-      position: "UI /UX Designer",
+      position: "UI/UX Designer",
       duration: "2024 - Present",
     },
     {
       company: "Sema Numerique",
-      position: "UI /UX Designer, Web Designer, Mobile Dev & desktop dev",
-      duration: " Avril 2023 - 2024 ",
+      position: "UI/UX Designer, Web Designer, Mobile Dev & Desktop Dev",
+      duration: "Avril 2023 - 2024",
     },
     {
-      company: "Mawe ",
+      company: "Mawe",
       position: "Desktop Developer",
-      duration: "janvier 2023 - mars  2023 ",
+      duration: "Janvier 2023 - Mars 2023",
     },
     {
-      company: "sema Numerique",
-      position: "UI /UX & Developpeur Mobile",
-      duration: "2022 - 2023 ",
+      company: "Sema Numerique",
+      position: "UI/UX & Développeur Mobile",
+      duration: "2022 - 2023",
     },
     {
-      company: "CRES .",
-      position: "Developpeur Mobile",
-      duration: "mars 2022 - septembre 2022 ",
+      company: "CRES",
+      position: "Développeur Mobile",
+      duration: "Mars 2022 - Septembre 2022",
     },
     {
       company: "Freelance",
-      position: "Developpeur DeskTop",
-      duration: "juin 2021 - decembre 2021",
+      position: "Développeur Desktop",
+      duration: "Juin 2021 - Décembre 2021",
     },
     {
-      company: "Freelance ",
-      position: "Graphismes ",
-      duration: "ui /ux desiner ",
+      company: "Freelance",
+      position: "Graphismes",
+      duration: "UI/UX Designer",
     },
   ],
 };
+
 // Education data
 const education = {
   icon: "/assets/resume/cap.svg",
-  title: "My Education ",
+  title: "My Education",
   description:
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil mollitia excepturi, corporis amet fugit voluptate, in quaerat dolores, nulla magni hic aspernatur velit aliquam atque repellat provident autem deserunt accusantium?",
   items: [
     {
       institution: "Tupedane Institut",
-      degree: "Social Diplôme d'Etat ",
-      duration: " 2018 ",
+      degree: "Social Diplôme d'Etat",
+      duration: "2018",
     },
     {
       institution: "ISIG-GOMA",
-      degree: "G1 en Informatique de gestion ",
-      duration: " 2019-2020 ",
+      degree: "G1 en Informatique de gestion",
+      duration: "2019-2020",
     },
     {
       institution: "ISIG-GOMA",
-      degree: "G2 En Informatique de gestion ",
-      duration: " 2020-2021 ",
+      degree: "G2 En Informatique de gestion",
+      duration: "2020-2021",
     },
     {
       institution: "ISIG-GOMA",
-      degree: "G3 En Informatique de gestion ",
-      duration: " 2021-2022 ",
+      degree: "G3 En Informatique de gestion",
+      duration: "2021-2022",
     },
     {
       institution: "ISIG-GOMA",
-      degree: "licences En Informatique de gestion ",
-      duration: " 2022-2023 ",
+      degree: "Licences En Informatique de gestion",
+      duration: "2022-2023",
     },
     {
       institution: "CFPI-GOMA",
       degree: "Web programming",
-      duration: " 2023",
+      duration: "2023",
     },
   ],
 };
-//Skills
+
+// Skills
 const skills = {
   title: "My skills",
   description:
@@ -133,51 +138,78 @@ const skills = {
   skillList: [
     {
       icon: <FaHtml5 />,
-      name: "html 5",
+      name: "HTML 5",
     },
     {
-        icon: <FaCss3 />,
-        name: "Css 3",
+      icon: <FaCss3 />,
+      name: "CSS 3",
     },
     {
-        icon: <FaFlutter />,
-        name: "Flutter ",
-      },
-      {
-        icon: <FaJs />,
-        name: "javascript",
-      },
-      {
-        icon: <FaCsharp />,
-        name: "C#",
-      },
-      {
-        icon: <FaReact />,
-        name: "React.js",
-      },
-      {
-        icon: <SiNextdotjs />,
-        name: "next.js",
-      },
-      {
-        icon: <SiTailwindcss />,
-        name: "tailwind.css ",
-      },
-      {
-        icon: <FaNodeJs />,
-        name: "node.js ",
-      },
-      {
-        icon: <FaFigma />,
-        name: "Figma ",
-      },
+      icon: <FaFlutter />,
+      name: "Flutter",
+    },
+    {
+      icon: <FaJs />,
+      name: "JavaScript",
+    },
+    {
+      icon: <FaCsharp />,
+      name: "C#",
+    },
+    {
+      icon: <FaReact />,
+      name: "React.js",
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: "Next.js",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "Tailwind CSS",
+    },
+    {
+      icon: <FaNodeJs />,
+      name: "Node.js",
+    },
+    {
+      icon: <FaFigma />,
+      name: "Figma",
+    },
   ],
 };
 
-import { Tabs, TabsContent,TabsList, TabsTrigger } from "@components/ui/Tabs";
-import {motion} from "framer-motion"; 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { motion } from "framer-motion";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const resume = () => {
-  return <motion.div initial={{opacity: 0}} animate={{opacity:1, transition}}>resume page</motion.div>;
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }}
+      className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs defaultValue="experiences" className="flex flex-col xl:flex-row gap-[60px]">
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="Experinences">Experiences</TabsTrigger>
+            <TabsTrigger value="education">Educations</TabsTrigger>
+            <TabsTrigger value="skills">Competences</TabsTrigger>
+            <TabsTrigger value="about">A propos de Moi</TabsTrigger>
+          </TabsList>
+          {/*content*/}
+          <div>content</div>
+        </Tabs>
+      </div>
+    </motion.div>
+  );
 };
+
 export default resume;
