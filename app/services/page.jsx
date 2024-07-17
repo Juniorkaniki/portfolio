e1d028bef41443d82dddf8028b1b9c7adbe4e4c5
Juniorkaniki <juniorkaniki00@gmail.com>
2024-07-17 20:25:18 +0200

    retouche sur le menu services

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -47,9 +47,7 @@ const Services = () => {
             return (
               <div key={index} className="flex-1 flex flex-col justify-center gap-6 group" >
                 <div className="w-full flex justify-between items-center"> 
-                <div className="text-5xl font-extrabold">
-                    <span className="text-outline text-transparent group-hover:text-blue-500 transition-all duration-500">{service.num}</span>
-                  </div>
+                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500  ">{service.num}</div>
                   <Link href={service.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-blue-500 transition-all duration-500 flex justify-center items-center hover:-rotate-45">
                     <BsArrowDownRight className="text-primary text-3xl"/>
                   </Link>
